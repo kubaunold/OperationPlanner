1cdc3c4888063c928f74599c45ed0cf7ba4af625
Kuba Unold <241160@student.pwr.edu.pl>
2021-11-18 20:48:46 +0100

    Added loop icon for searching.

--- OperationPlanner/Form1.Designer.cs
+++ OperationPlanner/Form1.Designer.cs
@@ -33,20 +33,29 @@ namespace OperationPlanner
             this.button_connect = new System.Windows.Forms.Button();
             this.label_logger = new System.Windows.Forms.Label();
             this.richTextBox_logger = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_database)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView_database
             // 
             this.dataGridView_database.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_database.Location = new System.Drawing.Point(12, 82);
+            this.dataGridView_database.Location = new System.Drawing.Point(34, 655);
             this.dataGridView_database.Name = "dataGridView_database";
             this.dataGridView_database.Size = new System.Drawing.Size(468, 359);
             this.dataGridView_database.TabIndex = 0;
             // 
             // button_connect
             // 
-            this.button_connect.Location = new System.Drawing.Point(12, 12);
+            this.button_connect.Location = new System.Drawing.Point(34, 585);
             this.button_connect.Name = "button_connect";
             this.button_connect.Size = new System.Drawing.Size(142, 64);
             this.button_connect.TabIndex = 1;
@@ -57,7 +66,7 @@ namespace OperationPlanner
             // label_logger
             // 
             this.label_logger.AutoSize = true;
-            this.label_logger.Location = new System.Drawing.Point(483, 66);
+            this.label_logger.Location = new System.Drawing.Point(505, 639);
             this.label_logger.Name = "label_logger";
             this.label_logger.Size = new System.Drawing.Size(40, 13);
             this.label_logger.TabIndex = 3;
@@ -66,18 +75,90 @@ namespace OperationPlanner
             // 
             // richTextBox_logger
             // 
-            this.richTextBox_logger.Location = new System.Drawing.Point(486, 82);
+            this.richTextBox_logger.Location = new System.Drawing.Point(508, 655);
             this.richTextBox_logger.Name = "richTextBox_logger";
             this.richTextBox_logger.Size = new System.Drawing.Size(359, 359);
             this.richTextBox_logger.TabIndex = 4;
             this.richTextBox_logger.Text = "";
             this.richTextBox_logger.TextChanged += new System.EventHandler(this.richTextBox_logger_TextChanged);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(939, 191);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(222)))), ((int)(((byte)(233)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patient information";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(52, 219);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(844, 247);
+            this.panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 10F);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(222)))), ((int)(((byte)(233)))));
+            this.button1.Location = new System.Drawing.Point(19, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 10F);
+            this.textBox1.Location = new System.Drawing.Point(535, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::OperationPlanner.Properties.Resources.magnifying_glass;
+            this.pictureBox1.Location = new System.Drawing.Point(726, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 677);
+            this.ClientSize = new System.Drawing.Size(939, 847);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.richTextBox_logger);
             this.Controls.Add(this.label_logger);
             this.Controls.Add(this.button_connect);
@@ -86,6 +167,11 @@ namespace OperationPlanner
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_database)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,6 +183,12 @@ namespace OperationPlanner
         private System.Windows.Forms.Button button_connect;
         private System.Windows.Forms.Label label_logger;
         private System.Windows.Forms.RichTextBox richTextBox_logger;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
